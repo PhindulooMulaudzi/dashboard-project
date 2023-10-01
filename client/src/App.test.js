d66aa526d6d935acb290154d613996db2025e4d6
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-10-01 17:47:56 +0200

    tests failing due to antd import

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test ('renders learn react link', () => {
+  render (<App />);
+  const linkElement = screen.getByText (/learn react/i);
+  expect (linkElement).toBeInTheDocument ();
+});
+
+/******Tests assuming the database is connect, we are not mocking****** */
+
+const request = require ('supertest');
+const app = require ('../../server/server');
+
+describe ('API Endpoints', () => {
+  test ('GET /mines should return status 200', async () => {
+    const response = await request (app).get ('/mines');
+    expect (response.status).toBe (200);
+  });
+
+  test ('GET /contact/:id should return status 200', async () => {
+    const response = await request (app).get ('/contact/1'); // this mine id exists ("Diamond Delve Mine")
+    expect (response.status).toBe (200);
+  });
 });
